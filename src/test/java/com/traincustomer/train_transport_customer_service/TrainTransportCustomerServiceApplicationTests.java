e8001173433642ebcc4c81204943caaff2f449ca
Petar Miljkovic <petar.miljkovic1993@gmail.com>
2024-09-30 16:37:53 +0200

    Initial commit

---
+++ src/test/java/com/traincustomer/train_transport_customer_service/TrainTransportCustomerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.traincustomer.train_transport_customer_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainTransportCustomerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
